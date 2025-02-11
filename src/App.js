258a86e771ba2de96d74f6c4cbf02b0356d5d6c2
Huthasan Odugu <nanihuthasun@gmail.com>
2025-02-11 11:52:52 +0530

    second task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { MantineProvider } from '@mantine/core';
+import Testing from './Components/Testing';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MantineProvider>
+<Testing/>
+      </MantineProvider>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
